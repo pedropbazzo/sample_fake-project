1cf803ac24cada0c27a6aa0015834645650fcf91
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2019-04-23 10:23:51 +0200

    Clean up files

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true $*
-mvn sonar:sonar $*
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dmaven.test.failure.ignore=true $*
